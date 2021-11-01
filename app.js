51c09db5142d5720d608f7a6b55075606e05a07b
code_max <ogbujigodwinmax@gmail.com>
2021-11-01 23:08:53 +0100

    created server

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
 const cors = require('cors');
 
 const app = express();
